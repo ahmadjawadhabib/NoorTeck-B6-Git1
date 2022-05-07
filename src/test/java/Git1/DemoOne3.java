8e88b06b3b428482cc36eedc6e5cc3b7f5c59786
ahmadjawadhabib <ahmadjawadhabib@localhost>
2022-05-06 21:52:37 -0400

    demo 3

---
+++ src/test/java/Git1/DemoOne3.java
@@ -0,0 +1,16 @@
+package Git1;
+
+public class DemoOne3 {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Git Demo 1....");
+		System.out.println("Demo Two");
+		
+		System.out.println("Demo Three ");
+		
+		
+		System.out.println("abc");
+		System.out.println("050632022");
+	}
+}
